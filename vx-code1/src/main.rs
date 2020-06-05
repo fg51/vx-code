10eaab605b7defbd4c596144425cd0fe05685d8d
kflange <zknsftk1msrk@gmail.com>
2020-06-05 10:51:52 +0900

    Added: xi (wip)

--- vx-code1/src/main.rs
+++ vx-code1/src/main.rs
@@ -1,5 +1,10 @@
+use std::cell::RefCell;
 use std::env;
+use std::fs::File;
 use std::io::{stdin, stdout, Write};
+use std::process::exit;
+use std::rc::Rc;
+use std::thread;
 
 use anyhow::Result;
 use env_logger as logger;
@@ -14,6 +19,9 @@ use termion::raw::IntoRawMode;
 use termion::screen::AlternateScreen;
 use termion::{clear, cursor};
 
+// use xi_rpc::{Peer, RpcLoop};
+use xi_rpc::RpcLoop;
+
 mod xi;
 
 fn main() -> Result<()> {
@@ -25,22 +33,64 @@ fn main() -> Result<()> {
         .version(crate_version!())
         .about(crate_description!())
         .arg(Arg::with_name("file").help("The file to open"));
-    let _file_path = app.get_matches().value_of("file");
+    let file_path = app.get_matches().value_of("file");
 
-    let stdin = stdin();
-    let mut stdout = AlternateScreen::from(stdout().into_raw_mode()?);
+    let (client_to_core_writer, core_to_client_reader, client_to_client_writer) =
+        xi::run_xi();
+    let mut front_event_loop = RpcLoop::new(client_to_core_writer);
+    let raw_peer = front_event_loop.get_raw_peer();
 
-    write!(stdout, "{}", clear::All)?;
-    write!(stdout, "{}", cursor::Goto(1, 1))?;
-    write!(stdout, "Hello World")?;
-    stdout.flush()?;
+    let child = thread::spawn(move || {});
 
-    for event in stdin.events() {
-        if event? == Event::Key(Key::Ctrl('c')) {
-            info!("recieved: ctrl + c");
-            return Ok(());
+    let child = thread::spawn(move || {
+        let layout = TermionLayout::new();
+
+        let styles: Rc<RefCell<Box<dyn Styles>>> =
+            Rc::new(RefCell::new(Box::new(TermionStyles::new())));
+
+        let mut event_handler = EventController::new(Box::new(layout), styles.clone());
+        front_event_loop
+            .mainloop(|| core_to_client_reader, &mut event_handler)
+            .unwrap();
+    });
+
+    let mut input_controller = InputController::new(
+        Box::new(TermionKeyboard::from_reader(stdin())),
+        client_to_client_writer,
+        // &config,
+    );
+
+    match file_path {
+        Some(file_path) => {
+            if let Err(err) = input_controller.open_file(&raw_peer, file_path) {
+                eprintln!("failed to open {}: {}", file_path, err);
+                exit(1);
+            }
+        }
+        None => {
+            eprintln!("failed to open the file. need fail path.");
+            exit(1);
         }
     }
+
+    if let Err(err) = input_controller.start_keyboard_event_loop(&raw_peer) {
+        eprintln!("an error occured: {}", err);
+        exit(1);
+    }
+
+    child.join().unwrap();
+
+    //write!(stdout, "{}", clear::All)?;
+    //write!(stdout, "{}", cursor::Goto(1, 1))?;
+    //write!(stdout, "Hello World")?;
+    //stdout.flush()?;
+
+    //for event in stdin.events() {
+    //    if event? == Event::Key(Key::Ctrl('c')) {
+    //        info!("recieved: ctrl + c");
+    //        return Ok(());
+    //    }
+    //}
 
     Ok(())
 }
--- vx-code1/src/xi.rs
+++ vx-code1/src/xi.rs
@@ -1,9 +1,97 @@
 use std::io::{self, BufRead, Read, Write};
+use std::sync::mpsc::{channel, Receiver, Sender};
+use std::thread;
 
-use serde_json::Value;
+use log::error;
+use serde_json::{json, Value};
 use xi_core_lib::XiCore;
 use xi_rpc::RpcLoop;
 
+pub struct Writer(Sender<String>);
+
+impl Write for Writer {
+    fn write(&mut self, buf: &[u8]) -> io::Result<usize> {
+        let s = String::from_utf8(buf.to_vec()).unwrap();
+        self.0
+            .send(s)
+            .map_err(|err| io::Error::new(io::ErrorKind::Other, format!("{:?}", err)))
+            .map(|_| buf.len())
+    }
+
+    fn flush(&mut self) -> io::Result<()> {
+        Ok(())
+    }
+}
+
+pub struct Reader(Receiver<String>);
+
+impl Read for Reader {
+    fn read(&mut self, _buf: &mut [u8]) -> io::Result<usize> {
+        unreachable!("didn't expect xi-rpc to call read");
+    }
+}
+
+// Note: we don't properly implement BufRead, only the stylized call patterns
+// used by xi-rpc.
+impl BufRead for Reader {
+    fn fill_buf(&mut self) -> io::Result<&[u8]> {
+        unreachable!("didn't expect xi-rpc to call fill_buf");
+    }
+
+    fn consume(&mut self, _amt: usize) {
+        unreachable!("didn't expect xi-rpc to call consume");
+    }
+
+    fn read_line(&mut self, buf: &mut String) -> io::Result<usize> {
+        let event = match self.0.recv() {
+            Ok(s) => s,
+            Err(_) => return Ok(0),
+        };
+
+        if &event == r#"{"method":"command","params":{"method":"exit"}}"# {
+            // It receive a close commmand from the writer indicating the chan
+            // should be closes. The event is sent by the InputController when
+            // the user ask to quit the program.
+            //
+            // This method is required because the chan producers a shared between
+            // The InputController and the EventController threads and it's
+            // impossible for the InputController to close the EventController
+            // channel.
+            //
+            // When the Reader receives the command, it close the channel between
+            // the InputController which lead to the following steps in order:
+            // - The channel between the the InputController and the Core close itself.
+            // - The Core event loop stop itself safely.
+            // - The channel between the Core and the EventController close itself.
+            // - The the EventController event loop stop itself safely.
+            // - The main exit.
+            return Ok(0);
+        }
+
+        buf.push_str(&event);
+        Ok(event.len())
+    }
+}
+
+pub struct ClientToClientWriter(Writer);
+
+impl ClientToClientWriter {
+    pub fn send_rpc_notification(&mut self, method: &str, params: &Value) {
+        let raw_content =
+            match serde_json::to_vec(&json!({"method": method, "params": params})) {
+                Ok(raw) => raw,
+                Err(err) => {
+                    error!("failed to create the notification {}: {}", method, err);
+                    return;
+                }
+            };
+
+        match self.0.write(&raw_content) {
+            Ok(_) => (),
+            Err(err) => error!("failed to send the notification {}: {}", method, err),
+        };
+    }
+}
 pub fn run_xi() -> (Writer, Reader, ClientToClientWriter) {
     let mut core = XiCore::new();
 
