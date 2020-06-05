61ae57ddd883c0016289c71b8bfdc361ec0110c2
kflange <zknsftk1msrk@gmail.com>
2020-06-05 09:56:48 +0900

    Added: stdout

--- vx-code/src/main.rs
+++ vx-code/src/main.rs
@@ -1,3 +1,4 @@
+use std::env;
 use std::io::Result;
 use std::io::{stdin, stdout, Write};
 
@@ -7,13 +8,21 @@ use log::info;
 use termion::event::{Event, Key};
 use termion::input::TermRead;
 use termion::raw::IntoRawMode;
+use termion::screen::AlternateScreen;
+use termion::{clear, cursor};
 
 fn main() -> Result<()> {
+    env::set_var("RUST_LOG", "debug");
     logger::init();
     info!("start");
 
     let stdin = stdin();
-    let mut stdout = stdout().into_raw_mode()?;
+    let mut stdout = AlternateScreen::from(stdout().into_raw_mode()?);
+
+    write!(stdout, "{}", clear::All)?;
+    write!(stdout, "{}", cursor::Goto(1, 1))?;
+    write!(stdout, "Hello World")?;
+    stdout.flush()?;
 
     for event in stdin.events() {
         if event? == Event::Key(Key::Ctrl('c')) {
