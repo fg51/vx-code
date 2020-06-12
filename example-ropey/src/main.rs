0477e4464fce0e30e04df83d0c836c8e85c3ba7d
kflange <zknsftk1msrk@gmail.com>
2020-06-12 19:18:55 +0900

    update

--- example-ropey/src/main.rs
+++ example-ropey/src/main.rs
@@ -1,4 +1,5 @@
 use std::fs::File;
+use std::io::Write;
 
 use ropey::Rope;
 
@@ -10,13 +11,19 @@ fn main() {
 }
 
 fn run() -> Result<(), std::io::Error> {
-    let mut text = Rope::from_reader(File::open("a.txt")?)?;
+    let text = Rope::from_reader(File::open("a.txt")?)?;
 
     println!("{}", text.line(100));
 
     let start_idx = text.line_to_char(100);
     let end_idx = text.line_to_char(101);
     println!("{}", text.slice(start_idx..end_idx));
+
+    for i in text.lines() {
+        print!("{}", i);
+    }
+    let mut stdout = std::io::stdout();
+    stdout.flush()?;
 
     Ok(())
 }
