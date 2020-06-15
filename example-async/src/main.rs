1541c9d8cb0bbb1003af61d58373fad909cd8082
kflange <zknsftk1msrk@gmail.com>
2020-06-15 09:39:42 +0900

    update

--- example-async/src/main.rs
+++ example-async/src/main.rs
@@ -24,11 +24,51 @@ fn hello() -> warp::filters::BoxedFilter<()> {
     warp::path("hello").boxed()
 }
 
-fn name() -> warp::filters::BoxedFilter<(String,)> {
+fn name() -> warp::filters::BoxedFilter<(Name,)> {
     warp::path::param().boxed()
 }
 
-async fn greet_handler(name: String) -> Result<impl warp::Reply, warp::Rejection> {
+async fn greet_handler(name: Name) -> Result<impl warp::Reply, warp::Rejection> {
     let reply = format!("hello {}", name);
     Ok(warp::reply::html(reply))
 }
+
+#[derive(Clone, Debug)]
+struct Name(String);
+
+impl Name {
+    pub fn new(name: &str) -> Result<Self, String> {
+        let size = name.chars().count();
+        if size < 1 || size > 10 {
+            return Err("required: less than 10 strings".to_string());
+        }
+        if name.chars().any(|c| !c.is_ascii_alphabetic()) {
+            return Err("enable: A-Z, a-z.".to_string());
+        }
+        Ok(Name(name.to_string()))
+    }
+}
+
+impl std::str::FromStr for Name {
+    type Err = String;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        Name::new(s)
+    }
+}
+
+impl std::fmt::Display for Name {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{}", self.0)
+    }
+}
+
+mod test {
+    use super::*;
+
+    #[test]
+    fn test_name() {
+        let ok_value = "Async";
+        assert!(Name::new(ok_value).is_ok());
+    }
+} /* test */
